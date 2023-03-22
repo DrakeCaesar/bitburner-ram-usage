c095ef38b75bfc222794a72f1e4be0e0c220a734
Dominik Cisár <dominik.cisar@gmail.com>
2023-03-22 21:48:25 +0100

    added functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,75 +1,122 @@
 import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log(
-        'Congratulations, your extension "line-counter" is now active!'
-    );
+    console.log('Congratulations, your extension "ram-counter" is now active!');
 
     let activeEditor = vscode.window.activeTextEditor;
+    let ramCostDecoration: vscode.TextEditorDecorationType | undefined;
 
-    let lineCountDecoration: vscode.TextEditorDecorationType | undefined;
+    interface FunctionRamCost {
+        type: string;
+        name: string;
+        cost: number;
+    }
 
-    function updateLineCount() {
+    let ramCosts: FunctionRamCost[] = [];
+    const baseCost = 1.6;
+
+    async function parseRamCosts() {
+        if (
+            ramCosts.length === 0 &&
+            activeEditor &&
+            vscode.workspace.workspaceFolders
+        ) {
+            const rootFolder = vscode.workspace.workspaceFolders[0].uri.fsPath;
+            const definitionsPath = path.join(
+                rootFolder,
+                "NetscriptDefinitions.d.ts"
+            );
+
+            if (fs.existsSync(definitionsPath)) {
+                const fileContent = await fs.promises.readFile(
+                    definitionsPath,
+                    "utf-8"
+                );
+                const functionRegex =
+                    /(?:RAM cost:|@remarks)[\s*]+(?:RAM cost:)? ?(\d+(?:\.\d+)?) ?GB[\s\S]+? (?:(\w+)\(.*\):|readonly (\w+))/g;
+
+                let match;
+                while ((match = functionRegex.exec(fileContent)) !== null) {
+                    ramCosts.push({
+                        type: match[2] ? "function" : "interface",
+                        name: match[2] ?? match[3],
+                        cost: parseFloat(match[1]),
+                    });
+                }
+            }
+            console.log(JSON.stringify(ramCosts, null, 2));
+        }
+    }
+
+    async function updateRamUsage() {
         if (activeEditor && activeEditor.document.lineCount > 0) {
-            console.log("function is running");
             const firstLine = activeEditor.document.lineAt(0);
-            if (firstLine.text.startsWith("import { NS }")) {
-                if (lineCountDecoration) {
-                    activeEditor.setDecorations(lineCountDecoration, []); // Clear previous decoration
+            if (firstLine.text.startsWith('import { NS } from "@ns"')) {
+                await parseRamCosts();
+
+                let totalRamCost = baseCost;
+                const documentText = activeEditor.document.getText();
+
+                for (const ramCost of ramCosts) {
+                    const regex = new RegExp(`\\b${ramCost.name}\\(`, "g");
+                    const matches = documentText.match(regex);
+                    if (matches) {
+                        totalRamCost += ramCost.cost;
+                        console.log(
+                            `${matches[0]} : ${ramCost.name} : ${ramCost.cost}`
+                        );
+                    }
                 }
 
-                const lineCount = activeEditor.document.lineCount;
+                if (ramCostDecoration) {
+                    activeEditor.setDecorations(ramCostDecoration, []); // Clear previous decoration
+                }
+
                 const position = new vscode.Position(
                     0,
                     firstLine.range.end.character
                 );
-                lineCountDecoration =
+                ramCostDecoration =
                     vscode.window.createTextEditorDecorationType({
                         after: {
-                            contentText: ` (${lineCount} lines)`,
+                            contentText: ` (Total RAM usage: ${totalRamCost.toFixed(
+                                2
+                            )} GB)`,
                             fontStyle: "italic",
                             color: "gray",
                         },
                     });
-                activeEditor.setDecorations(lineCountDecoration, [
+                activeEditor.setDecorations(ramCostDecoration, [
                     new vscode.Range(position, position),
                 ]);
-            } else if (lineCountDecoration) {
-                activeEditor.setDecorations(lineCountDecoration, []); // Clear decoration if the file does not match the required pattern
+            } else if (ramCostDecoration) {
+                activeEditor.setDecorations(ramCostDecoration, []); // Clear decoration if the file does not match the required pattern
             }
         }
     }
 
     if (activeEditor) {
-        updateLineCount();
+        updateRamUsage();
     }
-
-    vscode.window.onDidChangeActiveTextEditor(
-        (editor) => {
-            activeEditor = editor;
-            if (editor) {
-                updateLineCount();
-            }
-        },
-        null,
-        context.subscriptions
-    );
 
     vscode.workspace.onDidOpenTextDocument(
         (document) => {
             activeEditor = vscode.window.activeTextEditor;
             if (activeEditor && activeEditor.document === document) {
-                updateLineCount();
+                updateRamUsage();
             }
         },
         null,
         context.subscriptions
     );
 
-    vscode.workspace.onDidChangeTextDocument(
-        (event) => {
-            if (activeEditor && event.document === activeEditor.document) {
-                updateLineCount();
+    vscode.window.onDidChangeActiveTextEditor(
+        (editor) => {
+            activeEditor = editor;
+            if (editor) {
+                updateRamUsage();
             }
         },
         null,
